e459dcfee54422a077dce3f6f067370410c8da82
Vedant Singh <singhvedant1701@gmail.com>
2024-02-08 15:19:20 +0530

    Added count of word

--- src/Q9.java
+++ src/Q9.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Q9 {
     // Create a program to find an input word in a input string.
-    // IF the word is found, print "Word found" else throw a custom exception "Word not found".
-    public static void main(String[] args) {
+    // IF the word is found, give the frequency of the word else throw a custom exception "Word not found".
+    public static void main(String[] args) throws WordNotFoundException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the line:");
-        String input = scanner.nextLine();
-        System.out.println("Input the word:");
-        String word =  scanner.nextLine();
-        try {
-            if (input.contains(word)) {
-                System.out.println("Word found");
-            } else {
-                throw new WordNotFoundException("Word not found");
+        System.out.println("Enter a string: ");
+        String inputString = scanner.nextLine();
+        System.out.println("Enter a word to find: ");
+        String word = scanner.nextLine();
+        int frequency = 0;
+        for (String s : inputString.split(" ")) {
+            if (s.equals(word)) {
+                frequency++;
             }
-        } catch (WordNotFoundException e) {
-            System.out.println(e);
         }
+        if (frequency == 0) {
+            throw new WordNotFoundException("Word not found");
+        }
+        System.out.println("Frequency of " + word + " is " + frequency);
     }
 }
